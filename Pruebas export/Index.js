53683683457aa26b2169603432f496a7c55ba79b
danielgarcia223u-bit <danielgarcia223u@gmail.com>
2026-03-09 17:15:32 -0400

    clase 9 de marzo

---
+++ Pruebas export/Index.js
@@ -0,0 +1,5 @@
+const numran = Math.random() * 10;
+
+const numran2 = Math.ceil(numran) - 1;
+console.log(numran2);
+export { numran2 };
---
+++ Pruebas export/main.js
@@ -0,0 +1,9 @@
+import { numran2 } from "./Index.js";
+
+if (numran2 > 5) {
+    console.log("El número es mayor a 5");
+    console.log("El número es: " + numran2);
+} else {
+    console.log("El número es menor o igual a 5");
+    console.log("El número es: " + numran2);
+}
